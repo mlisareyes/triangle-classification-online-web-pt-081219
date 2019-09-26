c1eebf8046519cc19967c027223d577f89be692c
Monica R <mlisareyes@gmail.com>
2019-09-26 02:00:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,33 +9,18 @@ class Triangle
   end
   
   def kind
-    validate_triangle
+    
     if a == b && b == c
       :equilateral
     elsif
       a == b || b == c || a == c
         :isosceles
-    else
-      :scalene
+    elsif
+      a != b && b != c
+      
     end
   end
   
-  def validate_triangle
-    validate_positive_length
-    validate_triangle_inequality
-  end
-
-  def validate_positive_length
-    [a, b, c].each do |side|
-      raise TriangleError if side <= 0
-    end
-  end
-
-  def validate_triangle_inequality
-    if (a + b) <= c
-      raise TriangleError
-    end
-  end
   
   class TriangleError < StandardError
   end
