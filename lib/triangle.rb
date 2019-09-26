9f4b169e3153a2d44dda3e58cea9dd56e40c2ccf
Monica R <mlisareyes@gmail.com>
2019-09-26 01:49:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Triangle
   
   attr_accessor :a, :b, :c
@@ -34,3 +35,49 @@ class Triangle
   end
   
 end
+=======
+class Triangle
+  
+  attr_accessor :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+  
+  def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif
+      a == b || b == c || a == c
+        :isosceles
+    else
+      :scalene
+    end
+  end
+  
+  def validate_triangle
+    validate_positive_length
+    validate_triangle_inequality
+  end
+
+  def validate_positive_length
+    [a, b, c].each do |side|
+      raise TriangleError if side <= 0
+    end
+  end
+
+  def validate_triangle_inequality
+    if (a + b) <= c
+      raise TriangleError
+    end
+  end
+end
+ 
+ 
+
+class TriangleError < StandardError
+end
+>>>>>>> b1b1a27423ac6fddf2f8f16ca8fe515a4b03c640
